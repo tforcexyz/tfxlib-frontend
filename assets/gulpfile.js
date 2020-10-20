c7e62ba30edf4aac429ba9e4dd690362f99a3781
T-Force Xyz <30133485+tforcexyz@users.noreply.github.com>
2020-10-20 19:02:20 +0700

    Bootstrap | TFX-109 [WEBRES] Create distribution for common packages: jQuery, Bootstrap, Font Awesome

--- assets/gulpfile.js
+++ assets/gulpfile.js
@@ -8,31 +8,51 @@ var rename = require('gulp-rename');
 var replace = require('gulp-replace');
 var minifyJs = require('gulp-uglify');
 
-task('cleanup', function(callback) {
-  del('build/**');
+task('bootstrap--3', function(callback) {
+  src('./node_modules/bootstrap--3/dist/css/**')
+    .pipe(dest('./build/bootstrap--3/css'));
+  src('./node_modules/bootstrap--3/dist/fonts/**')
+    .pipe(dest('./build/bootstrap--3/fonts'));
+  src('./node_modules/bootstrap--3/dist/js/bootstrap**')
+    .pipe(dest('./build/bootstrap--3/js'));
   callback();
 })
 
+task('bootstrap--4', function(callback) {
+  src('./node_modules/bootstrap--4/dist/css/**')
+    .pipe(dest('./build/bootstrap--4/css'));
+  src('./node_modules/bootstrap--4/dist/js/bootstrap**')
+    .pipe(dest('./build/bootstrap--4/js'));
+  callback();
+})
+
+task('bootstrap', series('bootstrap--3', 'bootstrap--4'))
+
 task('jquery--1', function(callback) {
   src('./node_modules/jquery--1/dist/**')
-    .pipe(dest('./build/jquery--1/js/'))
+    .pipe(dest('./build/jquery--1/js'));
   callback();
 })
 
 task('jquery--2', function(callback) {
   src('./node_modules/jquery--2/dist/**')
-    .pipe(dest('./build/jquery--2/js/'))
+    .pipe(dest('./build/jquery--2/js'));
   callback();
 })
 
 task('jquery--3', function(callback) {
   src('./node_modules/jquery--3/dist/**')
-    .pipe(dest('./build/jquery--3/js/'))
+    .pipe(dest('./build/jquery--3/js'));
+  callback();
+})
+
+task('remove_build', function(callback) {
+  del('./build');
   callback();
 })
 
 task('jquery', series('jquery--1', 'jquery--2', 'jquery--3'))
 
-task('build', parallel('jquery'))
+task('build', parallel('bootstrap', 'jquery'))
+task('clean', series('remove_build'))
 
-task('default', series('cleanup', 'build'))
